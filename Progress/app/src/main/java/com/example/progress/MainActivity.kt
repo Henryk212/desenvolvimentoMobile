de7b3ed09d7755c79b612009a39e650da69118b1
Gabriel <ga43031@gmail.com>
2024-08-30 13:49:36 -0300

    Lógica de recomeçar

--- Progress/app/src/main/java/com/example/progress/MainActivity.kt
+++ Progress/app/src/main/java/com/example/progress/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
@@ -37,69 +38,100 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProgressTheme {
-                val n = GenerateNumber()
-                Render(n)
+                var start by remember { mutableStateOf('y') }
+                var n by remember { mutableStateOf(GenerateNumber()) }
+                var clicks by remember { mutableStateOf(0) }
+                if (start == 'y') {
+                    Render(clicks, n, start, onClickDecrement = {clicks--},onClickIncrement = {clicks++},onRestart = {
+                        n = GenerateNumber()
+                        clicks = 0
+                        start = 'y'
+                    })
+                }
             }
         }
     }
 }
 
 @Composable
-fun Render(n : Int) {
-    Text(text = "$n",
-        color = Color.Black,
-        modifier = Modifier.padding(50.dp))
+fun Render(clicks : Int, n : Int, start : Char,
+           onClickIncrement: () -> Unit,
+           onClickDecrement: () -> Unit,
+           onRestart: () -> Unit) {
 
-    var count by remember { mutableStateOf(0) }
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(50.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(text = "Count: $count", color = Color.Black, fontSize = 24.sp, fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { count++ },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = Color.White)) {
-            Text(text = "Increment!", color = Color.White)
+    if (start == 'y') {
+        Text(text = "$n",
+            color = Color.Black,
+            modifier = Modifier.padding(50.dp))
+
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(50.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(text = "Count: $clicks", color = Color.Black, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { onClickIncrement() },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = Color.White)) {
+                Text(text = "Increment!", color = Color.White)
+
+            }
+
+            Button(onClick = { onClickDecrement() },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = Color.White),
+            ) {
+                Text(text = "Decrement!", color = Color.White)
+            }
+
+            if (clicks < (n  * 0.33)) {
+                Image(painter = painterResource(id = R.drawable.planta1), contentDescription = "Flower 1",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Crop)
+            }
+
+            if (clicks >= (n * 0.33) && clicks < (n * 0.66)) {
+                Image(painter = painterResource(id = R.drawable.planta2), contentDescription = "Flower 2",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Crop)
+            }
+
+            if (clicks >= (n * 0.66) && clicks < (n * 0.99)) {
+                Image(painter = painterResource(id = R.drawable.planta3), contentDescription = "Flower 3",
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.Crop)
+            }
+
+            if (clicks == n) {
+                Image(painter = painterResource(id = R.drawable.sun), contentDescription = "Sun",
+                    modifier = Modifier,
+                    contentScale = ContentScale.Crop)
+
+                Column (
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(50.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "Play again?")
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Button(onClick = { onRestart() },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = Color.White),
+                    ) {
+                        clicks == 0
+                        Text(text = "Yes")
+
+                    }
+                }
+            }
 
         }
-
-        Button(onClick = { count-- },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = Color.White),
-        ) {
-            Text(text = "Decrement!", color = Color.White)
-        }
-
-        if (count < (n  * 0.33)) {
-            Image(painter = painterResource(id = R.drawable.planta1), contentDescription = "Flower 1",
-                modifier = Modifier
-                    .fillMaxSize(),
-                contentScale = ContentScale.Crop)
-        }
-
-        if (count >= (n * 0.33) && count < (n * 0.66)) {
-            Image(painter = painterResource(id = R.drawable.planta2), contentDescription = "Flower 2",
-                modifier = Modifier
-                    .fillMaxSize(),
-                contentScale = ContentScale.Crop)
-        }
-
-        if (count >= (n * 0.66) && count < (n * 0.99)) {
-            Image(painter = painterResource(id = R.drawable.planta3), contentDescription = "Flower 3",
-                modifier = Modifier
-                    .fillMaxSize(),
-                contentScale = ContentScale.Crop)
-        }
-
-        if (count == n) {
-            Image(painter = painterResource(id = R.drawable.sun), contentDescription = "Sun",
-                modifier = Modifier
-                    .fillMaxSize(),
-                contentScale = ContentScale.Crop)
-        }
-
     }
 }
 
